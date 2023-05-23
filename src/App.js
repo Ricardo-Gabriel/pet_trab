3e0bfd0128a674bce188838c134ece295606f02b
Ricardo-Gabriel <69832153+Ricardo-Gabriel@users.noreply.github.com>
2023-05-23 15:22:28 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Container from './components/layout/Container';
+import Home from './components/pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Navbar />
+      
+      <Container>
+        <Routes>
+            <Route exact path='./'  element={<Home />}></Route>
+                
+        </Routes>
+      </Container>
+
+      <Footer />
+    </Router>
     </div>
   );
 }
---
+++ src/components/pages/Cadastro.js
@@ -0,0 +1,7 @@
+import styles from './Cadastro.module.css'
+
+export default function Cadastro(){
+    return(
+        <></>
+    )
+}
