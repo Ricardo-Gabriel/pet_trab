35d206fa24f1894b1e95587b2a621b18fe4c6b38
Ricardo-Gabriel <69832153+Ricardo-Gabriel@users.noreply.github.com>
2023-05-29 12:56:41 -0300

    form link Db ok

--- src/components/pages/Cadastro.js
+++ src/components/pages/Cadastro.js
@@ -1,14 +1,48 @@
 import ProjectForm from '../project/ProjectForm';
 import styles from './Cadastro.module.css';
-import Container from '../layout/Container';
+import Container from '../layout/Container'
+import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 
 export default function Cadastro(){
-    return(
+    
+    /**
+        const [clientes, setClientes] = useState([]);
+        
+      
+        const location = useLocation();
+      
+        let message = "";
+      
        
+        useEffect(() => {
+          setTimeout(() => {
+            fetch("http://localhost:5000/clientes", {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+              },
+            })
+              .then((resp) => resp.json())
+              .then((data) => {
+                setClientes(data);
+                console.log(data);
+              })
+              .catch((err) => console.log(err));
+          }, 300);
+        }, []);
+    
+    
+        function handleChange(e){
+            setClientes({...project,[e.target.name]: e.target.value})
+    
+    }*/
+    return(
+        
           <Container>
            
-             <ProjectForm></ProjectForm>
+             <ProjectForm  ></ProjectForm>
             
             
           </Container>  
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -2,55 +2,105 @@ import Container from "../layout/Container";
 import styles from "./ProjectForm.module.css";
 
 
+import React, { useState } from 'react';
+
 export default function ProjectForm() {
+  const [clientes, setClientes] = useState({
+    id: 0,
+    nome: '',
+    cpf: '',
+    rg: '',
+    endereco: '',
+    numero: '',
+    complemento: '',
+    bairro: '',
+    cidade: ''
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setClientes((prevCliente) => ({
+      ...prevCliente,
+      [name]: value
+    }));
+  };
+
+  const createPost = async (event) => {
+    event.preventDefault();
+
+    const novoCliente = {
+      ...clientes,
+      id: Date.now() // Defina um ID único aqui, por exemplo, usando a data atual
+    };
+
+    try {
+      await fetch('http://localhost:5000/clientes', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(novoCliente)
+      });
+      alert('Cliente cadastrado com sucesso!');
+      setClientes({
+        id: 0,
+        nome: '',
+        cpf: '',
+        rg: '',
+        endereco: '',
+        numero: '',
+        complemento: '',
+        bairro: '',
+        cidade: ''
+      });
+    } catch (error) {
+      console.error('Erro ao cadastrar o cliente:', error);
+      alert('Erro ao cadastrar o cliente. Por favor, tente novamente.');
+    }
+  };
+
   return (
+    <Container>
+      <div className={styles.formulario}>
+        <h1>Cadastro</h1>
+        <br />
+        <div className={styles.item}>
+          <label htmlFor="nome">Nome completo</label>
+          <input type="text" name="nome" id="nome" value={clientes.nome} onChange={handleChange}></input>
+        </div>
+        <div className={styles.item}>
+          <label htmlFor="cpf">CPF</label>
+          <input type="text" name="cpf" id="cpf" value={clientes.cpf} onChange={handleChange}></input>
+        </div>
+        <div className={styles.item}>
+          <label htmlFor="rg">RG</label>
+          <input type="text" name="rg" id="rg" value={clientes.rg} onChange={handleChange}></input>
+        </div>
+        <div className={styles.item}>
+          <label htmlFor="endereco">Rua</label>
+          <input type="text" name="endereco" id="endereco" value={clientes.endereco} onChange={handleChange}></input>
+        </div>
+        <div className={styles.item}>
+          <label htmlFor="numero">Nº</label>
+          <input type="text" name="numero" id="numero" value={clientes.numero} onChange={handleChange}></input>
+        </div>
+        <div className={styles.item}>
+          <label htmlFor="complemento">Complemento</label>
+          <input type="text" name="complemento" id="complemento" value={clientes.complemento} onChange={handleChange}></input>
+        </div>
+        <div className={styles.item}>
+          <label htmlFor="bairro">Bairro</label>
+          <input type="text" name="bairro" id="bairro" value={clientes.bairro} onChange={handleChange}></input>
+        </div>
+        <div className={styles.item}>
+          <label htmlFor="cidade">Cidade</label>
+          <input type="text" name="cidade" id="cidade" value={clientes.cidade} onChange={handleChange}></input>
+        </div>
 
-       <Container>
-          <div className={styles.formulario} >
-          <h1>Cadastro</h1>
-          <br />
-          <div className={styles.item}>
-            <label for="nome">Nome completo</label>
-            <input type="text" name="nome" id="nome"></input>
-          </div>
-          <div className={styles.item}>
-            <label for="cpf">CPF</label>
-            <input type="text" name="cpf" id="cpf"></input>
-          </div>
-    
-          <div className={styles.item}>
-            <label for="rg">RG</label>
-            <input type="text" name="rg" id="rg"></input>
-          </div>
-          <div className={styles.item}>
-            <label for="endereco">Rua</label>
-            <input type="text" name="endereco" id="endereco"></input>
-          </div>
-          <div className={styles.item}>
-            <label for="numero">Nº</label>
-            <input type="text" name="numero" id="numero"></input>
-          </div>
-          <div className={styles.item}>
-            <label for="complemento">Complemento</label>
-            <input type="text" name="complemento" id="complemento"></input>
-          </div>
-          <div className={styles.item}>
-            <label for="bairro">Bairro</label>
-            <input type="text" name="bairro" id="bairro"></input>
-          </div>
-          <div className={styles.item}>
-            <label for="Cidade">Cidade</label>
-            <input type="text" name="Cidade" id="Cidade"></input>
-          </div>
-    
-          <button type="submit" className="item">Cadastrar</button>
-          </div>  
-
-         
-        </Container>
-
-    
-    
-   
+        <button type="submit" className={styles.item} onClick={createPost}>Cadastrar</button>
+      </div>
+    </Container>
   );
 }
+
+
